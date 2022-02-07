4efa9887f36b115c279c5a00ce0797757465c6dd
Dahir Mohamed <dmoabdi98@gmail.com>
2022-02-07 16:16:51 +0000

    set up code done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
 import "./App.css";
 
 function App() {
-  return <h1>eroingoi</h1>;
+  return (
+    <div>
+      <header>
+        <h1 className="title">Luis Diasss</h1>
+        <h2 className="title">Full Stack Developer</h2>
+      </header>
+
+      <main>
+        <section>
+          <h3 className="title">My Projects</h3>
+
+          <div className="projectCards">
+            <div className="card">
+              <img
+                src="https://dwglogo.com/wp-content/uploads/2017/09/1460px-React_logo.png"
+                alt=""
+              />
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Excepturi, error quas. Cumque enim a illum illo ut! Optio
+                molestiae animi magni nisi eos atque, illo fuga fugiat.
+                Obcaecati, tenetur quo?
+              </p>
+              <a href="https://reactjs.org/">REACT</a>
+            </div>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
 }
 
 export default App;
